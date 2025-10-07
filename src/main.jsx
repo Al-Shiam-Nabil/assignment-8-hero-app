d438e34d5a79824b6a4ab17b80f8022fb123050a
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-10-08 00:18:30 +0600

    folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router'
+import { router } from './Routes/Routes'
 
-const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<h1>Alhamdulillah</h1>
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
